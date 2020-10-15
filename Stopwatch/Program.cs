202cbf3bfdae1884d44313779d2f3a1447e78faa
Duk Sriwilai <duk151@hotmail.com>
2020-10-15 11:55:29 +0200

    commited

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please type 'Start' to begin the stopwatch, 'Stop' to end or " + "'exit' to quit: ");
+            var userInput = Console.ReadLine();
+
+            //initialize an object to run stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+            bool isStarted = false;
+            
+            //If user types exit the progarm will terminate
+            while (userInput.ToLower() != "exit")
+            {
+                if (userInput.ToLower() == "start")
+                {
+                    stopwatch.Start();
+                    isStarted = true;
+                    Console.WriteLine("Now type 'stop' to end timer or 'start' to reset: ");
+                    userInput = Console.ReadLine();
+
+                }
+                else if (userInput.ToLower() == "stop")
+                {
+                    if (isStarted)
+                    {
+                        stopwatch.Stop();
+                        Console.WriteLine("Now type 'stop' again for new time or 'start' to reset: ");
+                        userInput = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You need to start the timer first. Please type 'start'.");
+                        Console.Write("Now type start: ");
+                        userInput = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You did not type one of the required words. Please try again.");
+                    userInput = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        DateTime startTime;
+        DateTime endTime;
+
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+            TimeSpan elpasedTime = endTime - startTime;
+            Console.WriteLine("The following " + elpasedTime + " has elpased");
+        }
+    }
+}
